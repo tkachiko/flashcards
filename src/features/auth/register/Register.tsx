ed60c5d7e14e416e80a30a9eca878cf910dde32b
tkachiko <tkachikos@gmail.com>
2023-01-02 19:59:32 +0300

    add basic layout in Register page

--- src/features/auth/register/Register.tsx
+++ src/features/auth/register/Register.tsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import styleContainer from './../../../main/ui/common/styles/Container.module.css'
+import style from './Register.module.css'
+import {SuperInputText} from '../../../main/ui/common/components/SuperInputText/SuperInputText'
+import {NavLink} from 'react-router-dom'
+import {PATH} from '../../../main/ui/routes/routes'
+import {SuperButton} from '../../../main/ui/common/components/SuperButton/SuperButton'
 
 export const Register = () => {
   return (
-    <div className={`${styleContainer.container}`}>
-      REGISTER
+    <div className={`${style.formWrapper} ${styleContainer.container}`}>
+      <div className={style.formContainer}>
+        <h1 className={style.heading}>Sign Up</h1>
+        <form className={style.form}>
+          <div className={style.email}>
+            <label htmlFor={'email'} className={style.label}>Email</label>
+            <SuperInputText id={'email'} name={'email'} type={'text'} />
+          </div>
+          <div className={style.password}>
+            <label htmlFor={'password'} className={style.label}>Password</label>
+            <SuperInputText id={'password'} name={'password'} type={'password'} />
+            <div className={style.showHidePassword}></div>
+          </div>
+          <div className={style.password}>
+            <label htmlFor={'password'} className={style.label}>Confirm password</label>
+            <SuperInputText id={'password'} name={'password'} type={'password'} />
+            <div className={style.showHidePassword}></div>
+          </div>
+          <SuperButton className={style.button}>Sign Up</SuperButton>
+        </form>
+        <div className={style.account}>Already have an account?</div>
+        <NavLink to={PATH.REGISTER}>Sign In</NavLink>
+      </div>
     </div>
   )
 }
