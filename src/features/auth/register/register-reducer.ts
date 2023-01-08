78ca096c871f7add6a9793d23482a2c00170ae30
tkachiko <tkachikos@gmail.com>
2023-01-08 23:46:32 +0300

    feat(reducer): implement set error when data is incorrect

--- src/features/auth/register/register-reducer.ts
+++ src/features/auth/register/register-reducer.ts
@@ -1,3 +1,11 @@
+import axios, { AxiosError } from 'axios'
+import { Dispatch } from 'redux'
+import { ThunkAction, ThunkDispatch } from 'redux-thunk'
+
+import { RootStateType } from '../../../app/store'
+
+import { registerAPI } from './register-api'
+
 const SET_ERROR = 'flashcards/register/SET_ERROR'
 
 const initialState = {
@@ -16,6 +24,33 @@ export const registerReducer = (state: InitialStateType = initialState, action: 
 // actions
 export const setErrorAC = (error: null | string) => ({ type: SET_ERROR, error } as const)
 
+// thunks
+export const createUserTC =
+  (email: string, password: string): ThunkAppDispatchType =>
+  async (dispatch: Dispatch) => {
+    try {
+      await registerAPI.createUser(email, password)
+    } catch (e) {
+      if (axios.isAxiosError(e)) {
+        const error = e as AxiosError<{ error: string }>
+
+        const finalError = error.response ? error.response.data.error : e.message
+
+        dispatch(setErrorAC(finalError))
+      } else {
+        dispatch(setErrorAC('An unexpected error occurred'))
+      }
+    }
+  }
+
 // types
 export type ActionsType = ReturnType<typeof setErrorAC>
 export type InitialStateType = typeof initialState
+
+export type ThunkAppDispatchType<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootStateType,
+  unknown,
+  ActionsType
+>
+export type AppThunk = ThunkDispatch<RootStateType, unknown, ActionsType>
