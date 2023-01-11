875b569d1714b59770b9ee96a4afaa61fa0c75cf
rybaaa <bolshayaryba22@gmail.com>
2023-01-11 11:59:50 +0300

    feat: add information about user

--- src/app/header/Header.tsx
+++ src/app/header/Header.tsx
@@ -1,13 +1,29 @@
 import * as React from 'react'
 
+import { useSelector } from 'react-redux'
+
 import itIncubator from '../../assets/images/It-incubator.svg'
+import avatar from '../../assets/images/person.png'
+import { RootStateType, useAppSelector } from '../store'
 
 import s from './Header.module.scss'
 export const Header = () => {
+  const isLoggedIn = useAppSelector<boolean>(state => state.auth.isLoggedIn)
+  const nickname = useSelector<RootStateType, string>(state => state.profile.profile.name)
+
   return (
     <div className={s.container}>
-      <img src={itIncubator} />
-      <button className={s.button}>Sign in</button>
+      <img src={itIncubator} alt={'incubator logo'} />
+      {isLoggedIn ? (
+        <div className={s.login_info}>
+          <span className={s.nickname}>{nickname}</span>
+          <div className={s.avatar}>
+            <img style={{ width: '36px', height: '36px' }} src={avatar} alt={'avatar'} />
+          </div>
+        </div>
+      ) : (
+        <button className={s.button}>Sign in</button>
+      )}
     </div>
   )
 }
