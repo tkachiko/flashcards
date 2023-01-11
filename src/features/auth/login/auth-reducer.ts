a0f501f40529d5bb58a7bea8e7d96e1b447a8971
Armazok <nikolai.borsin@mail.ru>
2023-01-11 21:38:55 +0500

    Refactor: CASE NAME

--- src/features/auth/login/auth-reducer.ts
+++ src/features/auth/login/auth-reducer.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosError } from 'axios'
+import { Dispatch } from 'redux'
 
 import { loginAPI } from '../../../api/login-api'
-import { ActionsType, AppThunk, LoginType, ThunkAppDispatchType } from '../../../common/types/types'
+import { ActionsType, LoginType, ThunkAppDispatchType } from '../../../common/types/types'
 import { setDataAC } from '../../profile/profile-reducer'
 import { setErrorAC } from '../register/register-reducer'
 
@@ -13,7 +14,7 @@ const initialState = {
 
 export const authReducer = (state = initialState, action: ActionsType) => {
   switch (action.type) {
-    case 'login/SET-IS-LOGGED-IN':
+    case LOGIN_SET_IS_LOGGED_IN:
       return { ...state, isLoggedIn: action.newValue }
     default:
       return state
@@ -25,7 +26,7 @@ export const setIsLoggedInAC = (newValue: boolean) =>
 
 export const LoginTC =
   (data: LoginType): ThunkAppDispatchType =>
-  async (dispatch: AppThunk) => {
+  async (dispatch: Dispatch<ActionsType>) => {
     try {
       const res = await loginAPI.login(data)
 
@@ -41,5 +42,11 @@ export const LoginTC =
       } else {
         dispatch(setErrorAC(`Native error ${err.message}`))
       }
+
+      // if (axios.isAxiosError<AxiosError<{ error: string }>>(err)) {
+      //   const erroR = err.response?.data ? err.response.data.error : err.message
+      //
+      //   dispatch(setErrorAC(erroR))
+      // }
     }
   }
