0aa570fe0bbba40ee86ac4e377226a982ae03a5e
tkachiko <tkachikos@gmail.com>
2023-01-14 15:15:00 +0300

    refactor: types import

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,8 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import { applyMiddleware, combineReducers, legacy_createStore as createStore } from 'redux'
-import thunkMiddleware, { ThunkAction, ThunkDispatch } from 'redux-thunk'
+import thunkMiddleware from 'redux-thunk'
 
-import { ActionsType } from '../common/types/types'
+import { AppThunk } from '../common/types/types'
 import { authReducer } from '../features/auth/login/auth-reducer'
 import { passwordReducer } from '../features/password/password-reducer'
 import { profileReducer } from '../features/profile/profile-reducer'
@@ -18,14 +18,6 @@ const rootReducer = combineReducers({
 
 export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
 export type RootStateType = ReturnType<typeof rootReducer>
-
-export type ThunkAppDispatchType<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootStateType,
-  unknown,
-  ActionsType
->
-export type AppThunk = ThunkDispatch<RootStateType, unknown, ActionsType>
 
 export const useAppDispatch = () => useDispatch<AppThunk>()
 export const useAppSelector: TypedUseSelectorHook<RootStateType> = useSelector
