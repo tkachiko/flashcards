5ffa5a0d20e4af18d65d4818bb7984a6e1f03a9d
Nikita1025 <nikita1025@mail.ru>
2023-01-17 21:41:17 +0300

    get packs

--- src/features/decks/CardsPack.tsx
+++ src/features/decks/CardsPack.tsx
@@ -11,7 +11,8 @@ import TableRow from '@mui/material/TableRow'
 
 import { useAppDispatch, useAppSelector } from '../../app/store'
 
-import { fetchPacks, packSelector } from './cardsPack-reducer'
+import { ActionSetting } from './ActionSetting/ActionSetting'
+import { addPackTC, fetchPacks, packSelector } from './cardsPack-reducer'
 
 export const CardsPack = () => {
   const pack = useAppSelector(packSelector)
@@ -22,35 +23,44 @@ export const CardsPack = () => {
     dispatch(fetchPacks())
   }, [])
 
+  const onClick = () => {
+    dispatch(addPackTC('вфывф'))
+  }
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {pack?.cardPacks.map((el, i) => (
-            <TableRow
-              key={`${el._id}-${i}`}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {el.name}
-              </TableCell>
-              <TableCell align="right">{el.cardsCount}</TableCell>
-              <TableCell align="right">{el.updated}</TableCell>
-              <TableCell align="right">{el.created}</TableCell>
-              <TableCell align="right">{el.name}</TableCell>
+    <>
+      <button onClick={onClick}>add pack</button>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Dessert (100g serving)</TableCell>
+              <TableCell align="right">Calories</TableCell>
+              <TableCell align="right">Fat&nbsp;(g)</TableCell>
+              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+              <TableCell align="right">Protein&nbsp;(g)</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {pack?.cardPacks.map((el, i) => (
+              <TableRow
+                key={`${el._id}-${i}`}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {el.name}
+                </TableCell>
+                <TableCell align="right">{el.cardsCount}</TableCell>
+                <TableCell align="right">{el.updated}</TableCell>
+                <TableCell align="right">{el.created}</TableCell>
+                <TableCell align="right">
+                  <ActionSetting />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   )
 }
