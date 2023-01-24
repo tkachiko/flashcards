6c0261253bd48ab20e4120102b37f7723c9ed620
tkachiko <tkachikos@gmail.com>
2023-01-24 08:37:32 +0300

    feat: empty pack page

--- src/features/cards/emptyPack/EmptyPack.tsx
+++ src/features/cards/emptyPack/EmptyPack.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { FC } from 'react'
 
-import { CardsHeader } from '../cardsHeader/CardsHeader'
+import Button from '@mui/material/Button'
 
-export const EmptyPack = () => {
+import { appStatusSelector } from '../../../app/app-reducer'
+import { useAppSelector } from '../../../app/store'
+
+import style from './EmptyPack.module.scss'
+
+type PropsType = {
+  onCreateCardHandler: () => void
+}
+
+export const EmptyPack: FC<PropsType> = ({ onCreateCardHandler }) => {
+  const loadingStatus = useAppSelector(appStatusSelector)
+
   return (
-    <div>
-      <CardsHeader />
+    <div className={style.container}>
+      <p>This pack is empty. Click add new card to fill this pack</p>
+      <Button
+        variant={'contained'}
+        type={'button'}
+        fullWidth
+        disableRipple
+        disabled={loadingStatus === 'loading'}
+        className={style.button}
+        onClick={onCreateCardHandler}
+      >
+        Add new card
+      </Button>
     </div>
   )
 }
