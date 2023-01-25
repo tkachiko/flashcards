074a866cffdc6693e5cd2a228bdd01cbffa565b3
rybaaa <bolshayaryba22@gmail.com>
2023-01-25 20:45:21 +0300

    fix: right title when no query in searching field

--- src/common/components/NotFoundSearching/NotFoundSearching.tsx
+++ src/common/components/NotFoundSearching/NotFoundSearching.tsx
@@ -7,9 +7,11 @@ type NotFoundSearchingPropsType = {
 }
 
 export const NotFoundSearching = (props: NotFoundSearchingPropsType) => {
+  const error = props.packName.length ? props.packName : 'this query'
+
   return (
     <div className={style.wrapper}>
-      <h4>Not found for {props.packName}</h4>
+      <h4>Not found for {error}</h4>
     </div>
   )
 }
