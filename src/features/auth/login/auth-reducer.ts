4bf5afcbb1705c8da11a1e8aeece1ed90319f5f7
Nikita1025 <nekit1025l@gmail.com>
2023-02-02 20:22:55 +0300

    fix createAsyncThunk/auth-reducer

--- src/features/auth/login/auth-reducer.ts
+++ src/features/auth/login/auth-reducer.ts
@@ -1,10 +1,16 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { AxiosError } from 'axios'
 
 import { loginApi } from 'api/loginApi'
 import { setSubmittingAC } from 'app/app-reducer'
 import { RootStateType } from 'app/store'
-import { LoginType, ThunkAppDispatchType } from 'common/types/types'
+import {
+  AsyncThunkConfig,
+  LoginType,
+  LogOutResponseType,
+  ResponseType,
+  ThunkAppDispatchType,
+} from 'common/types/types'
 import { deleteUserDataAC, setDataAC } from 'features/profile/profile-reducer'
 import { errorMessage } from 'utils/error-utils'
 
@@ -26,33 +32,43 @@ export const authReducer = slice.reducer
 export const setIsLoggedInAC = slice.actions.setIsLoggedInAC
 
 //thunks
-export const LoginTC =
-  (data: LoginType): ThunkAppDispatchType =>
-  async dispatch => {
+export const LoginTC = createAsyncThunk<{ data: ResponseType }, LoginType, AsyncThunkConfig>(
+  'auth/login',
+  async (params, { dispatch, rejectWithValue }) => {
     dispatch(setSubmittingAC({ status: 'loading' }))
     try {
-      const res = await loginApi.login(data)
+      const res = await loginApi.login(params)
 
-      dispatch(setDataAC({ data: res.data }))
       dispatch(setSubmittingAC({ status: 'success' }))
       dispatch(setIsLoggedInAC({ newValue: true }))
+
+      return { data: res.data }
     } catch (e) {
       const error = e as Error | AxiosError
 
-      errorMessage(dispatch, error)
+      return rejectWithValue(errorMessage(dispatch, error))
     }
   }
-export const logoutTC = (): ThunkAppDispatchType => async dispatch => {
-  try {
-    await loginApi.logout()
-    dispatch(deleteUserDataAC())
-    dispatch(setIsLoggedInAC({ newValue: false }))
-  } catch (e) {
-    const error = e as Error | AxiosError
+)
 
-    errorMessage(dispatch, error)
+export const logoutTC = createAsyncThunk<{ data: LogOutResponseType }, AsyncThunkConfig>(
+  'auth/logout',
+  async (_, { dispatch, rejectWithValue }) => {
+    dispatch(setSubmittingAC({ status: 'loading' }))
+    try {
+      const res = await loginApi.logout()
+
+      dispatch(setIsLoggedInAC({ newValue: false }))
+      dispatch(setSubmittingAC({ status: 'success' }))
+
+      return { data: res.data }
+    } catch (e) {
+      const error = e as Error | AxiosError
+
+      return rejectWithValue(errorMessage(dispatch, error))
+    }
   }
-}
+)
 
 //types
 export type AuthActionType = ReturnType<typeof setIsLoggedInAC>
